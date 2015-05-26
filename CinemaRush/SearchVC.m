8ad56ebe033b92098131e38304d71ea0b473a259
darya busel <so.daria.96@gmail.com>
2015-05-26 19:41:07 +0300

    search page

---
+++ CinemaRush/SearchVC.m
@@ -0,0 +1,28 @@
+//
+//  SearchVC.m
+//  CinemaRush
+//
+//  Created by darya on 5/26/15.
+//  Copyright (c) 2015 BrightDays. All rights reserved.
+//
+
+#import "SearchVC.h"
+
+@interface SearchVC ()
+
+@end
+
+@implementation SearchVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
