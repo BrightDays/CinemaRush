55feb0614044d23af932b00d804a03eda9c6602d
darya busel <so.daria.96@gmail.com>
2015-06-04 03:54:59 +0300

    new

---
+++ CinemaRush/HomePageVC.m
@@ -0,0 +1,29 @@
+//
+//  HomePageVC.m
+//  CinemaRush
+//
+//  Created by darya on 5/26/15.
+//  Copyright (c) 2015 BrightDays. All rights reserved.
+//
+
+#import "HomePageVC.h"
+
+@interface HomePageVC ()
+
+@end
+
+@implementation HomePageVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// BLA bLA BLA
+
+@end
