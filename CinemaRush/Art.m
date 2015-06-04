916b6570034f85a7ee0b2b4952188e22ef8690de
darya busel <so.daria.96@gmail.com>
2015-06-04 04:01:18 +0300

    add Art.m base

---
+++ CinemaRush/Art.m
@@ -0,0 +1,24 @@
+//
+//  Art.m
+//  CinemaRush
+//
+//  Created by darya on 5/26/15.
+//  Copyright (c) 2015 Pops. All rights reserved.
+//
+
+#import "Art..h"
+
+@interface Art ()
+
+@end
+
+@implementation Art
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor greenColor];
+}
+ 
+
+
+@end
